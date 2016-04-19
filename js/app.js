20b6a127334c8c1ecbb414de66635a08448956e2
chicm <chicm.dev@gmail.com>
2016-04-19 17:59:08 +0800

    replace ngRoute with ui-router

--- js/app.js
+++ js/app.js
@@ -3,32 +3,37 @@
 
 // Declare app level module which depends on views, and components
 
-angular.module('myApp', ['ngRoute', 'LocalStorageModule', 'catalogservice'])
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-      .when('/home', {
-        templateUrl: "views/dashboard.html",
-        controller: "homeController"
-      })
-      .when('/items', {
-        templateUrl: "views/items.html",
-        controller: "itemController"
-      })
-      .when('/vms', {
-        templateUrl: "views/vms.html",
-        controller: "vmController"
-      })
-	  .when('/requests', {
-        templateUrl: "views/requests.html",
-        controller: "RequestController"
-      })
-      .otherwise({redirectTo: '/vms'});
-}])
+angular.module('myApp', ['ui.router', 'ui.bootstrap', 'LocalStorageModule', 'catalogservice'])
+.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/home');
+	$stateProvider
+		.state('home', {
+			url:'/home',
+			templateUrl: "views/dashboard.html",
+			controller: "homeController"
+		})
+		.state('items', {
+			url:'/items',
+			templateUrl: "views/items.html",
+			controller: "itemController"
+		})
+		.state('vms', {
+			url:'/vms',
+			templateUrl: "views/vms.html",
+			controller: "vmController"
+		})
+		.state('requests', {
+			url:'/requests',
+			templateUrl: "views/requests.html",
+			controller: "RequestController"
+		});
+     // .otherwise({redirectTo: '/vms'});
+})
 
 .controller('homeController', function() {
 
 })
-.controller('itemController', function($scope, CatalogService, localStorageService) {
+.controller('itemController', function($scope, $uibModal, CatalogService, localStorageService) {
 	var token = angular.fromJson(localStorageService.get('userInfo')).token;
 	console.log('itemController:' + token);
 	/*
@@ -45,11 +50,43 @@ angular.module('myApp', ['ngRoute', 'LocalStorageModule', 'catalogservice'])
 		}
 	}];*/
 	
+	$scope.open=function(item) {
+		var modalInstance = $uibModal.open({
+			animation:true,
+			templateUrl: 'views/requestItemModal.html',
+			controller: 'RequestItemController',
+			size: 'lg',
+			resolve: {
+				catalogItem: function () {
+				  return item;
+				}
+			}
+		});
+		modalInstance.result.then(function (catalogItem) {
+		  //$scope.selected = selectedItem;
+		}, function () {
+		  console.log('Modal dismissed at: ' + new Date());
+		});
+	};
+	
 	var items = CatalogService.listCatalogItems(token).then(function(content) {
 		$scope.items = content;
 		console.log("items:" + content);
 	}, function(res) {
 	});
+})
+.controller('RequestItemController', function($scope, $uibModalInstance, catalogItem) {
+	//$scope.openModal = function() {
+		console.log("opening modal: " + catalogItem);
+		$scope.item = catalogItem;
+		$scope.ok = function() {
+			$uibModalInstance.close($scope.item);
+		}
+		$scope.cancel = function() {
+			$uibModalInstance.dismiss();
+		}
+		//$modal.open({templateUrl:'views/requestItemModal.html'});
+	//};
 })
 .controller('vmController', function($scope, CatalogService, localStorageService) {
 	var token = angular.fromJson(localStorageService.get('userInfo')).token;
